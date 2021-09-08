0c936c994eb7f663d7df12c3305347a4138c5503
david-xander <ddelgado@a2sistemas.com>
2021-09-08 12:04:16 +0200

    checking nginx static

--- app/myWSGI.py
+++ app/myWSGI.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python
 import socket
 
+class MySocket:
+
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+    def __init__(self, sock=None):
+        self.MSGLEN = 2048
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < self.MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < self.MSGLEN:
+            chunk = self.sock.recv(min(self.MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+def do_something(client_socket):
+    pass
 
 def main():
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 8000
-    host_and_port = (socket.gethostname(), port)
-
+    # create an INET, STREAMing socket
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # bind the socket to a public host, and a well-known port
-    my_socket.bind(host_and_port)
-
-    # become a server socket
-    my_socket.listen(5)
-
-    # while True:
-    #     # accept connections from outside
-    #     (clientsocket, address) = my_socket.accept()
-    #     # now do something with the clientsocket
-    #     # in this case, we'll pretend this is a threaded server
-    #     ct = client_thread(clientsocket)
-    #     ct.run()
-
+    serversocket.bind(('127.0.0.1', 8000))
+    # become a server socket: queuing up to 5 connect requests
+    serversocket.listen(5)
+    
 if __name__ == '__main__':
     main()
